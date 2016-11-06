44636ae7128f8ff30bf78e8ec0a1e5f2059c7bed
Chris Boette <cboette@gmail.com>
2016-11-05 22:32:33 -0500

    Add function that crops an existing image.

---
+++ exhibit-00/save_img.go
@@ -0,0 +1,31 @@
+// https://github.com/lazywei/go-opencv/blob/master/samples/crop.go
+package main
+
+import (
+	"os"
+	"path"
+	"runtime"
+
+	opencv "github.com/lazywei/go-opencv/opencv"
+)
+
+func main() {
+	_, currentfile, _, _ := runtime.Caller(0)
+	filename := path.Join(path.Dir(currentfile), "test.jpg")
+	if len(os.Args) == 2 {
+		filename = os.Args[1]
+	}
+
+	image := opencv.LoadImage(filename)
+	if image == nil {
+		panic("LoadImage failed")
+	}
+	defer image.Release()
+
+	crop := opencv.Crop(image, 50, 50, 150, 150)
+	opencv.SaveImage("cropped.jpg", crop, 0)
+	crop.Release()
+
+	os.Exit(0)
+}
+
